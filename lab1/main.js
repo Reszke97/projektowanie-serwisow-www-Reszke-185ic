635cbfbcd40a2058b6923f8c770b35119267a722
Marcin Reszke <marcinreszke2@gmail.com>
2020-10-24 21:59:03 +0200

    lab1

--- lab1/main.js
+++ lab1/main.js
@@ -1,10 +1,25 @@
-function reset() {
-    var elements = document.getElementsByClassName("navbar-toggler");
-    for (var i = 0; i < elements.length; i++) {
-        if (elements[i].style.backgroundColor == "rgb(100, 100, 100)") {
-            elements[i].style.backgroundColor = '';
-        } else {
-            elements[i].style.backgroundColor = 'rgb(100, 100, 100)';
-        }
-    }
+var images = [];
+var nazwa = [];
+images[0] = "https://img.icons8.com/color/144/000000/bootstrap.png";
+images[1] = "https://img.icons8.com/color/144/000000/html-5.png";
+images[2] = "https://img.icons8.com/color/144/000000/javascript.png";
+images[3] = "https://img.icons8.com/color/144/000000/css3.png";
+nazwa[0] = "Bootstrap";
+nazwa[1] = "HTML5";
+nazwa[2] = "JavaScript";
+nazwa[3] = "CSS3";
+for (var i = 0; i < 4; i++) {
+    const el = document.createElement("span");
+    const el2 = document.createElement("span");
+    el2.style.setProperty("position", "absolute");
+    el2.style.setProperty("left", "50%");
+    el2.style.setProperty("color", "white")
+    el2.style.setProperty("transform", "translate(-50%, -50%)");
+    el2.style.setProperty("top", "0px");
+    el2.innerText = nazwa[i];
+    el.style.setProperty("position", "relative");
+    const span = document.querySelector(".textContainer"); //do tego elementu wstawię nowy element
+    el.innerHTML = '<img src="' + images[i] + '">';
+    el.appendChild(el2);
+    span.appendChild(el);
 }
